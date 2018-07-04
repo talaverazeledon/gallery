054d9ce153506cc65e909bfa9ca676d122b52a7e
JONATHAN <talaverazeledon@gmail.com>
2018-07-04 11:19:07 +0000

    gallery

--- js/scripts.js
+++ js/scripts.js
@@ -2,18 +2,173 @@
 
 $(document).ready(function () {
       
-      $("#uno").click(function(){
-        var currentWidth = $("#uno").css("width");
-        var currentheigth = $("#uno").css("height");
-        if (currentWidth && currentheigth == "200px") {
-                $("#uno").css({
-                    "height": "400px",
-                    "width":"400px"
-                });
-        }
-         else if (currentWidth && currentheigth == "400px") {
-          $("#uno").css("width", "200px");
-              $("#uno").css("height", "200px");
-         }
-      });
-      });
+$("#uno").click(function(){
+  var currentWidth = $("#uno").css("width");
+  var currentheigth = $("#uno").css("height");
+  if (currentWidth && currentheigth == "200px") {
+          $("#uno").css({
+              "height": "400px",
+              "width":"400px"
+          });
+  }
+   else if (currentWidth && currentheigth == "400px") {
+    $("#uno").css("width", "200px");
+        $("#uno").css("height", "200px");
+   }
+});
+$("#dos").click(function(){
+  var currentWidth = $("#dos").css("width");
+  var currentheigth = $("#dos").css("height");
+  if (currentWidth && currentheigth == "200px") {
+          $("#dos").css({
+              "height": "400px",
+              "width":"400px"
+          });
+  }
+   else if (currentWidth && currentheigth == "400px") {
+    $("#dos").css("width", "200px");
+        $("#dos").css("height", "200px");
+   }
+});
+$("#tres").click(function(){
+  var currentWidth = $("#tres").css("width");
+  var currentheigth = $("#tres").css("height");
+  if (currentWidth && currentheigth == "200px") {
+          $("#tres").css({
+              "height": "400px",
+              "width":"400px"
+          });
+  }
+   else if (currentWidth && currentheigth == "400px") {
+    $("#tres").css("width", "200px");
+        $("#tres").css("height", "200px");
+   }
+});
+$("#cuatro").click(function(){
+  var currentWidth = $("#cuatro").css("width");
+  var currentheigth = $("#cuatro").css("height");
+  if (currentWidth && currentheigth == "200px") {
+          $("#cuatro").css({
+              "height": "400px",
+              "width":"400px"
+          });
+  }
+   else if (currentWidth && currentheigth == "400px") {
+    $("#cuatro").css("width", "200px");
+        $("#cuatro").css("height", "200px");
+   }
+});
+$("#cinco").click(function(){
+  var currentWidth = $("#cinco").css("width");
+  var currentheigth = $("#cinco").css("height");
+  if (currentWidth && currentheigth == "200px") {
+          $("#cinco").css({
+              "height": "400px",
+              "width":"400px"
+          });
+  }
+   else if (currentWidth && currentheigth == "400px") {
+    $("#cinco").css("width", "200px");
+        $("#cinco").css("height", "200px");
+   }
+});
+$("#seis").click(function(){
+  var currentWidth = $("#seis").css("width");
+  var currentheigth = $("#seis").css("height");
+  if (currentWidth && currentheigth == "200px") {
+          $("#seis").css({
+              "height": "400px",
+              "width":"400px"
+          });
+  }
+   else if (currentWidth && currentheigth == "400px") {
+    $("#seis").css("width", "200px");
+        $("#seis").css("height", "200px");
+   }
+});
+$("#septima").click(function(){
+  var currentWidth = $("#septima").css("width");
+  var currentheigth = $("#septima").css("height");
+  if (currentWidth && currentheigth == "200px") {
+          $("#septima").css({
+              "height": "400px",
+              "width":"400px"
+          });
+  }
+   else if (currentWidth && currentheigth == "400px") {
+    $("#septima").css("width", "200px");
+        $("#septima").css("height", "200px");
+   }
+});
+$("#ocho").click(function(){
+  var currentWidth = $("#ocho").css("width");
+  var currentheigth = $("#ocho").css("height");
+  if (currentWidth && currentheigth == "200px") {
+          $("#ocho").css({
+              "height": "400px",
+              "width":"400px"
+          });
+  }
+   else if (currentWidth && currentheigth == "400px") {
+    $("#ocho").css("width", "200px");
+        $("#ocho").css("height", "200px");
+   }
+});
+$("#nueve").click(function(){
+  var currentWidth = $("#nueve").css("width");
+  var currentheigth = $("#nueve").css("height");
+  if (currentWidth && currentheigth == "200px") {
+          $("#nueve").css({
+              "height": "400px",
+              "width":"400px"
+          });
+  }
+   else if (currentWidth && currentheigth == "400px") {
+    $("#nueve").css("width", "200px");
+        $("#nueve").css("height", "200px");
+   }
+});
+$("#diez").click(function(){
+  var currentWidth = $("#diez").css("width");
+  var currentheigth = $("#diez").css("height");
+  if (currentWidth && currentheigth == "200px") {
+          $("#diez").css({
+              "height": "400px",
+              "width":"400px"
+          });
+  }
+   else if (currentWidth && currentheigth == "400px") {
+    $("#diez").css("width", "200px");
+        $("#diez").css("height", "200px");
+   }
+});
+$("#once").click(function(){
+  var currentWidth = $("#once").css("width");
+  var currentheigth = $("#once").css("height");
+  if (currentWidth && currentheigth == "200px") {
+          $("#once").css({
+              "height": "400px",
+              "width":"400px"
+          });
+  }
+   else if (currentWidth && currentheigth == "400px") {
+    $("#once").css("width", "200px");
+        $("#once").css("height", "200px");
+   }
+});
+
+$("#doce").click(function(){
+  var currentWidth = $("#doce").css("width");
+  var currentheigth = $("#doce").css("height");
+  if (currentWidth && currentheigth == "200px") {
+          $("#doce").css({
+              "height": "400px",
+              "width":"400px"
+          });
+  }
+   else if (currentWidth && currentheigth == "400px") {
+    $("#doce").css("width", "200px");
+        $("#doce").css("height", "200px");
+   }
+});
+});   
